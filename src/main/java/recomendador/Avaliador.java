7270c717166519e563f879f1c21b527d40bbe89b
jhonataribeiro <jhonata.ribeiro.ti@gmail.com>
2018-12-27 22:27:57 -0200

    implementar avaliador

---
+++ src/main/java/recomendador/Avaliador.java
@@ -0,0 +1,31 @@
+package recomendador;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.common.RandomUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author jribeiro
+ * @date 27/12/18
+ */
+public class Avaliador {
+    public static void main(String[] args) throws IOException, TasteException {
+
+        RandomUtils.useTestSeed();
+
+        File file = new File("dados.csv");
+        DataModel model = new FileDataModel(file);
+
+        RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+        RecommenderBuilder builder = new RecomendadorDeProdutosBuilder();
+        double error = evaluator.evaluate(builder,  null, model,0.9,1.0 );
+        System.out.println(error);
+    }
+}
